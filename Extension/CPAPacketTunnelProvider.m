210eb86a756cbf2d8016ffb9f7927a90ee329e13
Conrad Kramer <conrad@kramerapps.com>
2015-10-02 15:41:19 +0200

    Make packet tunnel provider actually capture packets

--- Extension/CPAPacketTunnelProvider.m
+++ Extension/CPAPacketTunnelProvider.m
@@ -21,32 +21,36 @@
 
 @dynamic protocolConfiguration;
 
-+ (void)load {
-    NSLog(@"STARTING");
-}
-
 - (void)handleAppMessage:(NSData *)messageData completionHandler:(nullable void (^)(NSData * __nullable responseData))completionHandler {
     
 }
 
 - (void)startTunnelWithOptions:(nullable NSDictionary<NSString *,NSObject *> *)options completionHandler:(void (^)(NSError * __nullable error))completionHandler {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    NEIPv4Settings *ipv4Settings = [[NEIPv4Settings alloc] initWithAddresses:@[@"192.168.1.2"] subnetMasks:@[@"255.255.255.0"]];
+    ipv4Settings.includedRoutes = @[[NEIPv4Route defaultRoute]];
+    
+    NEPacketTunnelNetworkSettings *settings = [[NEPacketTunnelNetworkSettings alloc] initWithTunnelRemoteAddress:@"8.8.8.8"];
+    settings.IPv4Settings = ipv4Settings;
+    settings.DNSSettings = [[NEDNSSettings alloc] initWithServers:@[@"192.168.1.2"]];
+    
+    __weak __typeof__(self) weakSelf = self;
+    [self setTunnelNetworkSettings:settings completionHandler:^(NSError *error) {
         completionHandler(nil);
         
         __block __weak void (^weakHandler)(NSArray<NSData *> *, NSArray<NSNumber *> *) = nil;
         void (^handler)(NSArray<NSData *> *, NSArray<NSNumber *> *) = ^(NSArray<NSData *> *packets, NSArray<NSNumber *> *protocols) {
             tunif *interface = tunif_new();
             for (NSData *packet in packets) {
+                NSLog(@"Packet! %@", packet);
                 tunif_input_packet(interface, packet.bytes, packet.length);
             }
             tunif_free(interface);
             
-            [self.packetFlow readPacketsWithCompletionHandler:weakHandler];
+            [weakSelf.packetFlow readPacketsWithCompletionHandler:weakHandler];
         };
         weakHandler = handler;
-        
-        [self.packetFlow readPacketsWithCompletionHandler:handler];
-    });
+        handler(nil, nil);
+    }];
 }
 
 - (void)stopTunnelWithReason:(NEProviderStopReason)reason completionHandler:(void (^)(void))completionHandler {
