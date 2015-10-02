0091fc48a282353e2e6c89c493c4856d1426db15
Conrad Kramer <conrad@kramerapps.com>
2015-10-02 15:41:49 +0200

    Start a default VPN configuration

--- iOS/ViewController.swift
+++ iOS/ViewController.swift
@@ -13,9 +13,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
         view.backgroundColor = UIColor.whiteColor()
-
-        NSLog("Test: \"\(CPAAppGroupIdentifier)\"")
+        
+        let start: (NETunnelProviderManager) -> (Void) = { (manager) -> Void in
+            do {
+                try manager.connection.startVPNTunnel()
+            } catch let error as NSError {
+                NSLog("Error! %@", error)
+            }
+        }
+        
+        let enableAndStart: (NETunnelProviderManager) -> (Void) = { (manager) -> Void in
+            if manager.enabled {
+                start(manager)
+            } else {
+                manager.enabled = true
+                manager.saveToPreferencesWithCompletionHandler({ (_) -> Void in
+                    start(manager)
+                })
+            }
+        }
+        NETunnelProviderManager.loadAllFromPreferencesWithCompletionHandler { (managers, _) -> Void in
+            if managers == nil || managers!.count == 0 {
+                let config = NETunnelProviderProtocol()
+                config.providerConfiguration = ["lol": 1]
+                config.providerBundleIdentifier = CPAExtensionBundleIdentifier
+                config.serverAddress = "somebridge"
+                
+                let manager = NETunnelProviderManager()
+                manager.protocolConfiguration = config
+                manager.localizedDescription = "Tor"
+                manager.saveToPreferencesWithCompletionHandler { (error) -> Void in
+                    if error != nil {
+                        NSLog("Error! %@", error!)
+                    }
+                    enableAndStart(manager)
+                }
+            } else {
+                enableAndStart(managers![0])
+            }
+        }
     }
 }
