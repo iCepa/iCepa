cc14f7b5f335ae32c3de7756754f6313b1846e1d
Conrad Kramer <conrad@kramerapps.com>
2015-10-02 13:03:39 +0200

    Added support for iOS to build script

--- Extension/tun2tor.sh
+++ Extension/tun2tor.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+
 if [[ ${ACTION:-build} = "build" ]]; then
-    cargo build --release --lib --target x86_64-apple-darwin
+    if [[ $PLATFORM_NAME = "macosx" ]]; then
+        TARGET_OS="darwin"
+    else
+        TARGET_OS="ios"
+    fi
+
+    for ARCH in $ARCHS
+    do
+        if [[ $(lipo -info "${BUILT_PRODUCTS_DIR}/libtun2tor.a" 2>&1) != *"${ARCH}"* ]]; then
+            cargo clean
+        fi
+    done
+
+    LIBRARIES=()
+    for ARCH in $ARCHS
+    do
+        cargo build --release --lib --target "${ARCH}-apple-${TARGET_OS}"
+        LIBRARIES+=("target/${ARCH}-apple-${TARGET_OS}/release/libtun2tor.a")
+    done
+
+    xcrun --sdk $PLATFORM_NAME lipo -create $LIBRARIES -output "${BUILT_PRODUCTS_DIR}/libtun2tor.a"
 elif [[ ${ACTION:-build} = "clean" ]]; then
     cargo clean
+    rm -f "${BUILT_PRODUCTS_DIR}/libtun2tor.a"
 fi
